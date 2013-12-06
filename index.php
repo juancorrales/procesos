5ab05cd1f14e64717ebdd2c3aa58d0986aebb772
juancorrales <juan.corrales@cerok.com>
2013-12-06 12:59:29 -0500

    creando rama de proceso de pqr

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        echo('Rama de proceso de pqr');
         ?>
     </body>
 </html>
